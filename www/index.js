52ae2c4823bc61724669d68ec9c1855bef49bbd5
Olli Huotari <olli.huotari@iki.fi>
2019-11-29 15:18:50 +0200

    JS learning finalized

--- www/index.js
+++ www/index.js
@@ -9,8 +9,8 @@ CartPole.prototype.observation = function () {
   return [this.x, this.velocity, this.pole_angle, this.pole_velocity]
 }
 window.CartPole = CartPole
-var cart_pole = CartPole.new()
-window.cart_pole = cart_pole
+// var cart_pole = CartPole.new()
+// window.cart_pole = cart_pole
 
 //////////////////////////////////////////////////////////////
 
@@ -19,9 +19,8 @@ class UI {
     this.transitions_user = []
     this.transitions_automatic = []
 
-    this.vis_real = []
-    this.vis_smoothed = []
-    this.vis_smoothed_tmp = []
+    this.vis_real = []; this.vis_smoothed = []; this.vis_smoothed_tmp = []
+    this.vis_eval_real = []; this.vis_eval_smoothed = []; this.vis_eval_smoothed_tmp = []
 
     this.episode_num = 0
     this.mode = ''
@@ -29,7 +28,7 @@ class UI {
     document.getElementById("ui_type_user").onclick = () => { this.userPlay() }
     document.getElementById("ui_type_train_show").onclick = () => { this.trainShow() }
     document.getElementById("ui_type_train").onclick = () => { this.train() }
-    document.getElementById("ui_type_eval").onclick = () => { this.eval() }
+    document.getElementById("ui_type_eval").onclick = () => { this.evalShow() }
     document.getElementById("ui_type_pause").onclick = () => { this.changeMode('pause') }
     this.vis_container = document.getElementById('vis-container')
     this.consoleLog('Initialized...')
@@ -52,39 +51,59 @@ class UI {
 
     for (let btn of document.getElementById('ui_type_buttons').children) { btn.classList.remove('active') }
     document.getElementById('ui_type_' + this.mode).classList.add('active')
-    tf.nextFrame()
   }
 
   plotScores() {
     tfvis.render.linechart(this.vis_container,
-      { values: [this.vis_real, this.vis_smoothed], series: ['Real', 'Smoothed'] }, { xLabel: 'Episode', yLabel: 'Score' }
-    );
+      // { values: [this.vis_real, this.vis_smoothed, this.vis_eval_real, this.vis_eval_smoothed],
+      //   series: ['Train', 'Train Smoothed', 'Eval', 'Eval Smoothed'] },
+      { values: [this.vis_smoothed, this.vis_eval_smoothed,],
+        series: ['Train', 'Eval',] },
+        { xLabel: 'Episode', yLabel: 'Score',
+          // seriesColors: ["rgb(255, 0, 200)", "rgb(0, 0, 200, 1)", "rgb(255, 153, 0, .5)", "rgb(255, 153, 0, 1)",] }
+          // seriesColors: ["#00ff00", "#00ff00","#00ff00","#00ff00"] }
+      }
+    )
   }
 
-  resetCartPole() {
+  cartPoleNew() {
     this.episode_num += 1
-    let score = cart_pole.step_count
-    // this.episode_scores.push(score)
+    return CartPole.new()
 
+  }
+  cartPoleReset(cart_pole) {
+    this.episode_num += 1
+    cart_pole.reset()
+  }
+
+  trainSaveScore(score) {
     this.vis_real.push({x: this.episode_num, y: score })
     this.vis_smoothed_tmp.push(score)
     this.vis_smoothed_tmp = this.vis_smoothed_tmp.slice(this.vis_smoothed_tmp.length - 25)
     this.vis_smoothed.push({ x: this.episode_num, y: this.vis_smoothed_tmp.reduce((prev, curr) => prev + curr) / this.vis_smoothed_tmp.length })
-
-    // for some reason continous tfvis.render causes memory bloating, so not doing too often
-    if (this.mode != 'train' || this.episode_num % 10 == 0){ this.plotScores() }
-    cart_pole.reset()
   }
 
-  async userDataTrain() {
-    this.consoleLog(`Training user-mimicin 4 epochs with sample size: ${this.transitions_user.length}.`)
-    policy_network.train_from_user(cart_pole, this.transitions_user)
+  evalSaveScore(score) {
+    this.vis_eval_real.push({x: this.episode_num, y: score })
+    this.vis_eval_smoothed_tmp.push(score)
+    this.vis_eval_smoothed_tmp = this.vis_eval_smoothed_tmp.slice(this.vis_eval_smoothed_tmp.length - 25)
+    this.vis_eval_smoothed.push({ x: this.episode_num, y: this.vis_eval_smoothed_tmp.reduce((prev, curr) => prev + curr) / this.vis_eval_smoothed_tmp.length })
+  }
+
+  async userDataTrain(cart_pole) {
+    this.consoleLog(`Training user-mimicking 4 epochs with sample size: ${this.transitions_user.length}.`)
+    policy_network.trainFromUser(cart_pole, this.transitions_user)
   }
 
   async userPlay() {
     this.changeMode('user')
     let timing = 0
+    const cart_pole = this.cartPoleNew()
+    for (let i = 0; i<5; i++) { await tf.nextFrame() }
     while (this.mode == 'user') {
+      if (cart_pole.step_count == 0) {
+        this.consoleLog('Game running, use left/right arrow-keys to move.')
+      }
       while (performance.now() - timing < 60 && this.mode == 'user') {
         await tf.nextFrame()
       }
@@ -107,9 +126,11 @@ class UI {
       if (cart_pole.step_count % 100 == 0){ this.userDataTrain() }
 
       if (reward < 1) {
-        this.consoleLog(`User episode ${this.episode_num}, score: ${cart_pole.step_count}. Start new game with keypress.`)
-        policy_network.train_from_user(cart_pole, this.transitions_user)
-        this.resetCartPole()
+        this.consoleLog(`User episode ${this.episode_num}, score: ${cart_pole.step_count}. Press arrow-keys to start.`)
+        this.plotScores()
+        policy_network.trainFromUser(cart_pole, this.transitions_user)
+        this.trainSaveScore(cart_pole.step_count)
+        this.cartPoleReset(cart_pole)
         cart_pole.draw(canvas)
 
         for (let i = 0; i < 15; i++) { await tf.nextFrame() }
@@ -124,11 +145,10 @@ class UI {
     this.userDataTrain()
   }
 
-  async trainEpisode() {
+  async trainEpisode(cart_pole) {
     let mode_original = this.mode
     const ep_trans = []
     let reward = 1;
-    cart_pole.reset()
 
     while (reward > 0 && cart_pole.step_count < 1000 && this.mode == mode_original) {
       let observation = cart_pole.observation()
@@ -143,7 +163,7 @@ class UI {
       // }
 
       // Boltzmann Approach for action selection
-      action = policy_network.predict_action(cart_pole, true)
+      action = policy_network.predictAction(cart_pole, true)
 
       reward = cart_pole.step(action - 1)
       ep_trans.push([observation, action, reward])
@@ -156,7 +176,6 @@ class UI {
 
     // this.consoleLog(`Trained episode ${this.episode_num}, score: ${cart_pole.step_count}, epsilon: ${Number(Math.round(epsilon + 'e2') + 'e-2')}`)
     this.consoleLog(`Trained episode ${this.episode_num}, score: ${cart_pole.step_count}`)
-    this.resetCartPole()
 
     // by default all rewards are 1, but discounted if we failed at the game
     // rewards range [-xxx..0] are discounted starting from 1 to -1 for the last action
@@ -176,40 +195,67 @@ class UI {
       this.transitions_automatic = this.transitions_automatic.slice(this.transitions_automatic.length - transitions_size)
     }
 
+    this.trainSaveScore(cart_pole.step_count)
+
     let i = 0
     if (this.mode == mode_original && i < 4) {
       i++
-      await policy_network.train_from_computer(this.transitions_automatic)
+      await policy_network.trainFromComputer(this.transitions_automatic)
+    }
+    if (this.mode == mode_original) {
+      if (this.episode_num % 10 == 0 || this.episode_num <= 1) {
+        await this.evalEpisode(cart_pole)
+      }
+      if (this.episode_num % 10 == 0 || this.episode_num < 10){
+        // for some reason continous tfvis.render causes memory bloating, so not doing too often
+        this.plotScores()
+      }
+      this.cartPoleReset(cart_pole)
     }
 
     if (this.mode == 'train') {
-      setTimeout(() => { if (this.mode==mode_original) { this.trainEpisode() } }, 10) // let some time for browser to handle ui stuff
+      setTimeout(() => { if (this.mode==mode_original) { this.trainEpisode(cart_pole) } }, 10) // let some time for browser to handle ui stuff
     }
   }
 
   async trainShow() {
     this.changeMode('train_show')
-    await tf.nextFrame()
-    while (this.mode == 'train_show') { await this.trainEpisode() }
+    for (let i = 0; i<5; i++) { await tf.nextFrame() }
+    const cart_pole = this.cartPoleNew()
+    while (this.mode == 'train_show') { await this.trainEpisode(cart_pole) }
   }
 
   async train() {
     this.changeMode('train')
-    await tf.nextFrame()
-    this.trainEpisode()
+    for (let i = 0; i<5; i++) { await tf.nextFrame() }
+    this.trainEpisode(this.cartPoleNew())
   }
 
-  async eval() {
+  async evalEpisode(cart_pole) {
+    let reward = 1
+    cart_pole.reset()
+    while (reward > 0) {
+      let force = policy_network.predictAction(cart_pole) - 1
+      reward = cart_pole.step(force)
+    }
+    this.evalSaveScore(cart_pole.step_count)
+    cart_pole.reset()
+  }
+
+  async evalShow() {
     this.changeMode('eval')
+    const cart_pole = this.cartPoleNew()
     while (this.mode == 'eval') {
-      let force = policy_network.predict_action(cart_pole) - 1
+      let force = policy_network.predictAction(cart_pole) - 1
       let reward = cart_pole.step(force)
       cart_pole.draw(canvas)
-      await tf.nextFrame()  // Unblock UI thread.
+      await tf.nextFrame() // Unblock UI thread.
 
       if (reward < 1) {
         this.consoleLog(`Evaluation episode ${this.episode_num}, score: ${cart_pole.step_count}`)
-        this.resetCartPole()
+        this.evalSaveScore(cart_pole.step_count)
+        this.cartPoleReset(cart_pole)
+        this.plotScores()
       }
     }
   }
@@ -247,7 +293,7 @@ class PolicyNetwork {
     return model;
   }
 
-  predict_action(cart_pole, randomized = false) {
+  predictAction(cart_pole, randomized = false) {
     return tf.tidy(() => {
       const logits = this.model.predict(tf.tensor2d([cart_pole.observation()]));
       if (randomized) {
@@ -260,7 +306,7 @@ class PolicyNetwork {
     })
   }
 
-  async train_from_user(transitions) {
+  async trainFromUser(transitions) {
     let x = [], y = []
 
     transitions.forEach(trans => {
@@ -278,9 +324,8 @@ class PolicyNetwork {
     return loss
   }
 
-  async train_from_computer(transitions) {
+  async trainFromComputer(transitions) {
     let minibatch_size = 32
-    // let loss_info = ''
     if (transitions.length >= minibatch_size) {
       let batch_x = []
       let batch_y_rewards = []
@@ -305,9 +350,7 @@ class PolicyNetwork {
       }
 
       return await this.model.fit(batch_x, tf.tensor(batch_y))
-      // loss_info = loss.history['loss'][0]
     }
-
   }
 
 }
